855abf64a3b7066e3f1bdd7caef089de91e2d739
therealsatya <satya@cs.cmu.edu>
2025-09-10 15:02:04 -0400

    Changed link to use AFS andrew.cmu.edu web site

--- publish_site.sh
+++ publish_site.sh
@@ -8,6 +8,8 @@ fi
 #bundle exec jekyll serve
 
 bundle exec jekyll build
-rsync -av _site/* satya@cernan.isr.cs.cmu.edu:/afs/cs.cmu.edu/project/coda-www/mcsa/
+rsync -av _site/* unix.andrew.cmu.edu:/afs/andrew/course/15/821/www/
+
+#rsync -av _site/* satya@cernan.isr.cs.cmu.edu:/afs/cs.cmu.edu/project/coda-www/mcsa/
 
 #curl "http://www.andrew.cmu.edu/cgi-bin/publish?FLAG=1&NAME=15-440"
